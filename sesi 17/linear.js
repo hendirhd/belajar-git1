e444ffab6532dd43dc072fa649900d91675489ce
hendirhd <68552398+hendirhd@users.noreply.github.com>
2022-06-21 17:02:59 +0700

    Add files via upload

---
+++ sesi 17/linear.js
@@ -0,0 +1,9 @@
+function linearSearch(arr,toFind) {
+    for (var i=0;arr.lenght;i++){
+        if(arr[i] == toFind) {
+            return i;
+        }
+    }
+    return null
+}
+var result = linearSearch 
